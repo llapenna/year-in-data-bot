14312d6141a3473e8a0fb1f6789160a600010e53
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-04-14 17:02:52 -0300

    feat(bot): add db interactions to start command

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -1,5 +1,7 @@
-import { Command } from "@/types/bot";
 import { Markup } from "telegraf";
+
+import { createUser, toggleNotify } from "@/db/user";
+import { Command } from "@/types/bot";
 
 enum KeyboardActions {
   NOTIFY = "START_NOTIFY",
@@ -8,8 +10,8 @@ enum KeyboardActions {
 export const START: Command = {
   command: "start",
   description: "Start command",
-  handler: (ctx) => {
-    if (!ctx.from) {
+  handler: async (ctx) => {
+    if (!ctx.from || !ctx.from.username) {
       ctx.reply(
         "Couldn't retrieve your information. Please try again later or contact our support!.",
       );
@@ -18,13 +20,18 @@ export const START: Command = {
     }
 
     // 1. Retrieve user ID
-    // const { id, username } = ctx.from;
+    const { id, username } = ctx.from;
 
     // 2. Store the ID with the user's username in the database
-    // TODO: database.user.create({ id, username })
+    const user = await createUser({ telegramId: id, username });
+
+    if (!user)
+      return ctx.reply(
+        "An error occurred while creating your account. Please try again later or contact our support!.",
+      );
 
     // 3. Send a welcome message with a button to allow the user to be notified each day o
-    ctx.reply(
+    return ctx.reply(
       "Welcome to Year in Data! 🎉.",
       Markup.inlineKeyboard([
         Markup.button.callback("Notify me daily", KeyboardActions.NOTIFY),
@@ -34,9 +41,16 @@ export const START: Command = {
   keyboard: [
     {
       action: KeyboardActions.NOTIFY,
-      handler: (ctx) => {
+      handler: async (ctx) => {
+        if (!ctx.from) {
+          ctx.reply(
+            "Couldn't retrieve your information. Please try again later or contact our support!.",
+          );
+
+          return;
+        }
         // 4. Set notify flag to true in the database'
-        // TODO: database.user.update({ notify:true })
+        await toggleNotify(ctx.from.id);
 
         ctx.reply("You will now receive daily updates from Year in Data! 🎉.");
       },
